40bce1e28997a35412e1e9e7316e3c17685a815b
aimandzhi <aigulmndzh@gmail.com>
2024-09-09 13:04:53 +0300

    add service auth layer

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -1,7 +1,69 @@
 package auth
 
-import `errors`
+import (
+	`context`
+	`errors`
+	`log/slog`
+	`time`
+
+	`github.com/dugtriol/auth-service/internal/domain/models`
+)
 
 var (
 	ErrInvalidCredentials = errors.New("invalid credentials")
 )
+
+type Auth struct {
+	log         *slog.Logger
+	usrSaver    UserSaver
+	usrProvider UserProvider
+	appProvider AppProvider
+	tokenTTL    time.Duration
+}
+
+type UserSaver interface {
+	SaveUser(
+		ctx context.Context,
+		email string,
+		passHash []byte,
+	) (uid int64, err error)
+}
+
+type UserProvider interface {
+	User(ctx context.Context, email string) (models.User, error)
+	IsAdmin(ctx context.Context, userID int64) (bool, error)
+}
+
+type AppProvider interface {
+	App(ctx context.Context, appID int) (models.App, error)
+}
+
+func New(
+	log *slog.Logger,
+	userSaver UserSaver,
+	userProvider UserProvider,
+	appProvider AppProvider,
+	tokenTTL time.Duration,
+) *Auth {
+	return &Auth{
+		usrSaver:    userSaver,
+		usrProvider: userProvider,
+		log:         log,
+		appProvider: appProvider,
+		tokenTTL:    tokenTTL,
+	}
+}
+
+func SaveUser(
+	ctx context.Context,
+	email string,
+	passHash []byte,
+) (uid int64, err error) {
+	panic("implement me")
+}
+
+func User(ctx context.Context, email string) (models.User, error) { panic("implement me") }
+
+func IsAdmin(ctx context.Context, userID int64) (bool, error) { panic("implement me") }
+
+func App(ctx context.Context, appID int) (models.App, error) { panic("implement me") }
